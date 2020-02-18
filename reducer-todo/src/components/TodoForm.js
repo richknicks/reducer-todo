f6107a19ad6fd4c471fa49e6cd5e73df3985dd17
richknicks <rlynch@gmail.com>
2020-02-18 18:13:07 -0500

    updated todoform with dispatch

--- reducer-todo/src/components/TodoForm.js
+++ reducer-todo/src/components/TodoForm.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState, useReducer } from "react";
+import { Reducer, initialState } from "../reducers/Reducer";
 
 
-const TodoForm = props => {
+const TodoForm = () => {
+    const [state, dispatch] = useReducer(Reducer, initialState);
+    console.log("The State",state, dispatch);
+    const [newTitleText, setNewTitleText] = useState('');
+    const handleChanges = e => {
+        setNewTitleText(e.target.value);
+      };
+      const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({type: 'ADD_TASK', payload: newTitleText})
+        console.log(state)
+    }
+      const handleClear = e => {
+        e.preventDefault();
+        dispatch({type: "CLEAR_COMPLETED", payload: newTitleText});
+    }
+      
+    
   return (
     <div>
-      <form onSubmit={}>
+      <form onSubmit={handleSubmit}>
           <label htmlFor="todoItems"><span>Enter Item Here: </span> 
-              <input type="text" id="todoItems" name="name" value={} onChange={}/>
-              <button type="submit">Add</button>
+              <input type="text" id="todoItems" name="newTitleText" value={newTitleText} onChange={handleChanges}/>
+              <button type="submit" onClick={()=>{
+                  dispatch({ type: 'UPDATE_TODO', payload: newTitleText });
+              }}>Add</button>
+              
           </label>
       </form>
-     
+      {state.map(item=> <p onClick={() => dispatch({ type: 'TOGGLE_COMPLETED' })}>{item.task}</p>)}
+      
     </div>
   );
 };
